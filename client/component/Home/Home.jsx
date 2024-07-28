71fca037cefb673a9166bfe9847f481fb3996220
Yashc885 <yashcoudhary88505342@gmail.com>
2024-07-29 01:44:20 +0530

    animation adde + vercel done

--- client/component/Home/Home.jsx
+++ client/component/Home/Home.jsx
@@ -1,16 +1,25 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { personal } from "../../Utils/Data/Personal.js";
 import Image from "next/image";
 import Link from "next/link";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
 import { MdDownload } from "react-icons/md";
 import { RiContactsFill } from "react-icons/ri";
 import { SiLeetcode } from "react-icons/si";
 import Main from "../../Utils/Assets/card.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+    });
+  }, []);
+
   return (
     <section className="pl-10 pr-10 relative flex flex-col items-center justify-between py-4 lg:py-12">
       <Image
@@ -21,18 +30,27 @@ function Home() {
         className="absolute -top-[98px] -z-10"
       />
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
-        <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
-          <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-12 gap-y-8 items-start">
+        <div className="flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10 order-2 lg:order-1">
+          <h1 
+            data-aos="fade-up" 
+            data-aos-duration="2000" 
+            data-aos-delay="500" 
+            className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]"
+          >
             Hello, <br />
             This is {' '}
-            <span className=" text-pink-500">{personal.name}</span>
+            <span className="text-pink-500">{personal.name}</span>
             {` , I'm a  `}
-            <span className=" text-[#16f2b3]">{personal.designation}</span>
+            <span className="text-[#16f2b3]">{personal.designation}</span>
             .
           </h1>
 
-          <div className="my-12 flex items-center gap-5">
+          <div          
+             data-aos="fade-up" 
+            data-aos-duration="2000" 
+            data-aos-delay="500" 
+            className="my-12 flex items-center gap-5">
             <Link
               href={personal.github}
               target='_blank'
@@ -57,34 +75,55 @@ function Home() {
           </div>
 
           <div className="flex items-center gap-3">
-            <Link href="/#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-              <button className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1 hover:gap-3">
+            <Link 
+              href="/#contact" 
+              className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600"
+            >
+              <button 
+              data-aos="fade-up" 
+            data-aos-duration="2000" 
+            data-aos-delay="500"
+
+              className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out md:font-semibold flex items-center gap-1 hover:gap-3">
                 <span>Contact me</span>
                 <RiContactsFill size={16} />
               </button>
             </Link>
 
-            <Link className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" role="button" target="_blank" href={personal.resume}
+            <Link 
+            data-aos="fade-up" 
+            data-aos-duration="2000" 
+            data-aos-delay="500"
+
+              className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" 
+              role="button" 
+              target="_blank" 
+              href={personal.resume}
             >
               <span>Resume</span>
               <MdDownload size={16} />
             </Link>
           </div>
-
         </div>
-        <div className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
+
+        <div 
+            data-aos="fade-up" 
+            data-aos-duration="2000" 
+            data-aos-delay="500" 
+            className="order-1 lg:order-2 bg-gradient-to-r from-[#0a0d37] to-[#0d1224] border-[#1b2c68a0] border rounded-lg relative">
           <div className="flex flex-row">
             <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
             <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
           </div>
           <div className="px-4 lg:px-8 py-5">
             <div className="flex flex-row space-x-2">
-            <div className="h-3 w-3 rounded-full bg-green-200"></div>
-            <div className="h-3 w-3 rounded-full bg-orange-400"></div>
+              <div className="h-3 w-3 rounded-full bg-green-200"></div>
+              <div className="h-3 w-3 rounded-full bg-orange-400"></div>
               <div className="h-3 w-3 rounded-full bg-red-400"></div>
             </div>
           </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+          <div
+             className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
             <code className="font-mono text-xs md:text-sm lg:text-base">
               <div className="blink">
                 <span className="mr-2 text-pink-500">const</span>
@@ -99,7 +138,7 @@ function Home() {
                 <span className="text-gray-400">{`',`}</span>
               </div>
               <div className="ml-4 lg:ml-8 mr-2">
-                <span className=" text-white">skills:</span>
+                <span className="text-white">skills:</span>
                 <span className="text-gray-400">{`['`}</span>
                 <span className="text-amber-300">React</span>
                 <span className="text-gray-400">{"', '"}</span>
@@ -165,15 +204,21 @@ function Home() {
                 <span className="mr-2 text-amber-300">&gt;=</span>
                 <span className="text-orange-400">5</span>
               </div>
-              <div><span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span></div>
-              <div><span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span></div>
-              <div><span className="text-gray-400">{`};`}</span></div>
+              <div>
+                <span className="ml-8 lg:ml-16 mr-2 text-gray-400">{`);`}</span>
+              </div>
+              <div>
+                <span className="ml-4 lg:ml-8 text-gray-400">{`};`}</span>
+              </div>
+              <div>
+                <span className="text-gray-400">{`};`}</span>
+              </div>
             </code>
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default Home;
