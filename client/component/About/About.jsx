a9179635782a56a2e3a8b34030b7b5a27f8e359a
Yashc885 <yashcoudhary88505342@gmail.com>
2024-07-29 01:07:07 +0530

    try 5

--- client/component/About/About.jsx
+++ client/component/About/About.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Link from "next/link";
 import { personal } from "./../../Utils/Data/Personal.js";
 import Image from "next/image";
-import Profile from "./../../Utils/Assets/Profile.jpg";
+import Profile from "./../../Utils/Assets/Profile.png";
 
 function AboutSection() {
   return (
